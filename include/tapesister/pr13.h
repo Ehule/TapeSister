a1d9c37ba99de8bf00ac45ff3bf7cc0e1ba46f0a
Ehule <62450456+Ehule@users.noreply.github.com>
2026-08-13 17:07:05 -0500

    Restore independent parent exploration

--- include/tapesister/pr13.h
+++ include/tapesister/pr13.h
@@ -1,7 +1,11 @@
 #ifndef TAPESISTER_PR13_H
 #define TAPESISTER_PR13_H
 
+#include "tapesister/sample.h"
+
 extern int ts_pr13_lock_request;
 void ts_pr13_set_lock_request(int enabled);
+int ts_pr13_new_parent(TsInstrument *instrument, int reseed,
+                       char *error, size_t error_size);
 
 #endif
